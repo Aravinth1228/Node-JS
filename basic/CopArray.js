50ce965b47dfa2351384f690aae483d962b85b60
Aravinth1228 <aravinthkalai1228@gmail.com>
2025-08-22 08:43:24 +0530

    add

--- basic/CopArray.js
+++ basic/CopArray.js
@@ -1,5 +1,14 @@
 const habbies1 = ['reading', 'writing', 'coding'];
-const habbies2 = habbies1.map(h => h.toUpperCase());
-habbies2.push('Dancing');
+// const habbies2 = habbies1.map(h => h.toUpperCase());
+// habbies2.push('Dancing');
 
+// console.log(habbies2);
+/*PS D:\Project\Node JS\Node-JS\basic> node .\CopArray.js
+[ 'READING', 'WRITING', 'CODING', 'Dancing' ]*/
+
+const habbies2 = habbies1.slice(); 
+habbies2.push('Dancing');
 console.log(habbies2);
+/*PS D:\Project\Node JS\Node-JS\basic> node .\CopArray.js
+[ 'reading', 'writing', 'coding', 'Dancing' ]*/
+ 
