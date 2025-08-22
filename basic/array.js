6d4561f26de0a1b7b943c3a7a31606d57492b8f1
Aravinth1228 <aravinthkalai1228@gmail.com>
2025-08-22 08:29:57 +0530

    Add

--- basic/array.js
+++ basic/array.js
@@ -1,5 +1,16 @@
-const sum = (a, b) => {
-    return a+b;
-};
+// const sum = (a, b) => {
+//     return a+b;
+// };
 
-console.log(sum(5, 10));
+// console.log(sum(5, 10));
+
+const habbies = ["Reading", "Traveling", "Cooking", "Gaming"];
+
+console.log(habbies.map(hobby=>'habbies :'+ hobby));
+/*PS D:\Project\Node JS\Node-JS\basic> node .\array.js
+[
+  'habbies :Reading',
+  'habbies :Traveling',
+  'habbies :Cooking',
+  'habbies :Gaming'
+]*/
