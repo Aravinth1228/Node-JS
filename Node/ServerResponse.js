a597fa061b54bb8fecea2922d224c576b5651f67
Aravinth1228 <aravinthkalai1228@gmail.com>
2025-08-23 12:55:00 +0530

    add

--- Node/ServerResponse.js
+++ Node/ServerResponse.js
@@ -11,8 +11,16 @@
 const http = require('http');
 
 const server = http.createServer( (req, res) => {
-    res.setHeader('Content-Type', 'text/html');
+
+    const url = req.url;
+    if(url === '/'){
+       res.setHeader('Content-Type', 'text/html');
+         res.write('<form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form>');
+        return res.end();
+    }
     res.write('<h1>Hello World</h1>');
+    res.end();   
+    
     
 });
 
