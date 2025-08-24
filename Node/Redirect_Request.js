21c300834b639c2ef59abb2b101dca6b6060b306
Aravinth1228 <aravinthkalai1228@gmail.com>
2025-08-24 19:55:39 +0530

    Add

---
+++ Node/Redirect_Request.js
@@ -0,0 +1,26 @@
+
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+
+    const url = req.url;
+    const method = req.method;
+    if (url === '/') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form>');
+        return res.end();
+    }
+    if (url === '/message' && method === 'POST') {
+        fs.writeFileSync('message.txt', 'DUMMY');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
+
+
+    res.write('<h1>Hello World</h1>');
+    res.end();
+});
+
+server.listen(3000);
