aab8d63d91cf131da264be35a6dfe7687122ef74
Aravinth1228 <aravinthkalai1228@gmail.com>
2025-08-24 21:20:03 +0530

    Add

--- Node/Stream&Buffer.js
+++ Node/Stream&Buffer.js
@@ -1,4 +1,3 @@
-
 const http = require('http');
 const fs = require('fs');
 
@@ -6,16 +5,23 @@ const server = http.createServer((req, res) => {
 
     const url = req.url;
     const method = req.method;
+   
     if (url === '/') {
         res.setHeader('Content-Type', 'text/html');
         res.write('<form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form>');
         return res.end();
     }
     if (url === '/message' && method === 'POST') {
-       req.on('data', (chunk) => {
-        console.log("Chunk received");
-        console.log(chunk);
-       })
+        const body = [];
+        req.on('data', (chunk) => {
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+        });
+        return;
     }
     res.write('<h1>Hello World</h1>');
     res.end();
